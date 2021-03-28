694b728e24b434ad702157ad2dcb2a5131b3af23
JohnArcher <radcliffe@gmx.de>
2021-03-28 18:11:48 +0200

    Go to definition improved and extented It also works for constants and inline functions now. Additionally there were many fixes made that where reported by mrupp. This commit also fixes #6 through adding a word pattern in the language-configuration.json. This was needed so the selected word when pressing F12 is taking special characters like #, ! and $ into account.

--- src/providers/definitionProvider.ts
+++ src/providers/definitionProvider.ts
@@ -10,8 +10,12 @@ export class HollywoodDefinitionProvider implements vscode.DefinitionProvider {
         Thenable<vscode.Definition>{
             return new Promise((resolve, reject) => {
 
-                // RegEx for finding normal function (first part) and variable (second part) definitions
-                const functionAndVariablesRE = /\b\b((Local|Global)(?:\s+))?(Function)|\b((Local|Global)(?:\s+))/i;
+                // TODO: is /i correct? I guess it is not correct if names are case sensitive in Hollywood (like in lua)
+                const functionRE = /\b(?:(Local|Global)(?:\s+))?(?:Function)(?:\s+([a-zA-Z_.:]+[.:])?([a-zA-Z_]\w*)\s*)?(\()([^)]*)(\))/i;
+                const inlineFunctionRE = /\b(?:(Local|Global)(?:\s+))?(?:([a-zA-Z_.:]+[.:])?([a-zA-Z_]\w*)\s*)?(?:\s*=\s*)(?:Function)(?:\s*)(\()([^)]*)(\))/i;
+                const variableRE = /(?!.*Function)(?<=(Local|Global)[ \t]*)\b(_|[a-zA-Z])(\w|!|\$)*((?=[ \t]*\=))*/i; // don't match, if the line contains the word "Function" anywhere
+                const constantsRE = /\b(?:Const(?:\s+))(#\S*)/i; // TODO: Vereinfachung; eventuell auch im DocumentSymbolProvider so nutzbar!
+                // TODO: variable definition like Local tt1, tt2, tt3
 
                 const range = document.getWordRangeAtPosition(position);
                 const selectedWord = document.getText(range);
@@ -21,13 +25,18 @@ export class HollywoodDefinitionProvider implements vscode.DefinitionProvider {
                 for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
                     let line = document.lineAt(lineNumber).text;
 
-                    if (line.search(functionAndVariablesRE) > -1) { // test if it is a normal function
+                    const name = functionRE.exec(line)?.[3] || inlineFunctionRE.exec(line)?.[3] || variableRE.exec(line)?.[0] || constantsRE.exec(line)?.[1];
+
+                    // if the evaluated name is equal to the selected word we have found the definition line
+                    if (name === selectedWord) {
 
                         /** Create a dynamic regex to find the selected word and its position on the line.
                           * It has to be a regex to find the _exact_ match, otherwise if selectedWord is "myFunc"
-                          * also "myFunc2" etc. would be found if you would use string.include() or string.indexOf()
+                          * then also "myFunc2" etc. would be found if you would use string.include() or string.indexOf().
+                          * It is also important to escape special characters like $ because this is a valid character
+                          * in a Hollywood variable but also a regex special character.
                           */
-                        const functionResult = new RegExp('\\b(' + selectedWord + ')\\b').exec(line);
+                        const functionResult = new RegExp(this.escapeRegExp(selectedWord)).exec(line);
 
                         if (functionResult) {
                             definitions.push({
@@ -43,5 +52,16 @@ export class HollywoodDefinitionProvider implements vscode.DefinitionProvider {
 
                 resolve(definitions);
             });
+    }
+
+    /**
+     * Escapes special regex characters in a string, so that this string can be used in a regex.
+     * @see https://stackoverflow.com/a/6969486/887930
+     *
+     * @param string String which may contain characters to be escaped
+     * @returns the escaped string
+     */
+    private escapeRegExp(string) {
+      return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
     }
 }

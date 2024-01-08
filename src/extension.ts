baf54637888ec045c0edf106e8a9ed3e1b3790b0
Micha J <radcliffe@gmx.de>
2024-01-08 23:23:53 +0100

    Update VS Code configuration and TypeScript settings; linting improvements

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,19 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, languages} from 'vscode';
 import { HollywoodDocumentSymbolProvider } from './providers/documentSymbolProvider';
 import { HollywoodDefinitionProvider } from './providers/definitionProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-  context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
-    {language: "hollywood"}, new HollywoodDocumentSymbolProvider()
+  context.subscriptions.push(languages.registerDocumentSymbolProvider(
+    { language: "hollywood" }, new HollywoodDocumentSymbolProvider()
   ));
 
-  context.subscriptions.push(vscode.languages.registerDefinitionProvider(
-    {language: "hollywood"}, new HollywoodDefinitionProvider()
+  context.subscriptions.push(languages.registerDefinitionProvider(
+    { language: "hollywood" }, new HollywoodDefinitionProvider()
   ));
+
 }
